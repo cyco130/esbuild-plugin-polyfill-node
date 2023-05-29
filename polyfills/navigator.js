a3777f379dccb1c200a4b1ed9fcab28dc0e5f16e
Fatih Aygün <cyco130@gmail.com>
2023-05-29 14:57:32 +0100

    navigator polyfill and finer control over globals (#10)
    * feat: navigator polyfill and finer control over globals
    
    * chore: update dependencies

---
+++ polyfills/navigator.js
@@ -0,0 +1,6 @@
+// These values are used by some of the JSPM polyfills
+export const navigator = {
+	deviceMemory: 8, // Highest allowed value
+	hardwareConcurrency: 8, // Fairly common default
+	language: "en-US", // Most common default
+};
